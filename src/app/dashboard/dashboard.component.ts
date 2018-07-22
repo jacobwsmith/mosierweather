9ef6efe228bc71fdc983feeb00761ff050036859
Jacob Smith <jacobwsmith@gmail.com>
2018-07-22 08:12:16 -0700

    chart config adjustments

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,11 +17,17 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.dashboardService.getSevenDayForecast().subscribe(({ windSpeed, temperature }) => {
-      console.log('res: ', JSON.stringify(windSpeed));
+      // console.log('res: ', JSON.stringify(windSpeed));
       this.chartOptions = {
         title: {
           text: 'Mosier Weather 7 Day Forecast',
           align: 'left'
+        },
+        credits: {
+          enabled: false
+        },
+        rangeSelector: {
+          enabled: false
         },
         time: {
           timezoneOffset: 8 * 60
@@ -54,7 +60,29 @@ export class DashboardComponent implements OnInit {
                 color: Highcharts.getOptions().colors[0]
               }
             },
-            height: 300
+            height: 300,
+            // plotBands: [{
+            //   color: '#FCFFC5', // Color value
+            //   from: 15, // Start of the plot band
+            //   to: 40, // End of the plot band
+            //   label: {
+            //     text: 'Kiteable', // Content of the label.
+            //     align: 'left', // Positioning of the label.
+            //     // Default to center. x: +10 // Amount of pixels the label will be repositioned according to the alignment.
+            //   }
+            // }],
+            plotLines: [{
+              color: 'red', // Color value
+              dashStyle: 'dash', // Style of the plot line. Default to solid
+              value: 15, // Value of where the line will appear
+              width: 2, // Width of the line
+              // label: {
+              //   text: 'Kiteable', // Content of the label.
+              //   align: 'left', // Positioning of the label.
+              //   // Default to center. x: +10 // Amount of pixels the label will be repositioned according to the alignment.
+              // }
+            }],
+            minRange: 20
           },
           {
             labels: {
